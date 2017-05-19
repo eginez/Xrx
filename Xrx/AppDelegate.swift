2de46ff4384e191c5d533faaf246a11fd3525c3a
Esteban <eginez@gmail.com>
2017-05-18 20:46:14 -0700

    Initial Commit

---
+++ Xrx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xrx
+//
+//  Created by Esteban on 5/18/17.
+//  Copyright © 2017 org.eginez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xrx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xrx
+//
+//  Created by Esteban on 5/18/17.
+//  Copyright © 2017 org.eginez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
